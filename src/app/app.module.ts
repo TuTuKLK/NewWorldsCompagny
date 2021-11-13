fd77e95cbbea403b45ff96166cff3f2705ba747e
TuTuKLK <geant.vert42@gmail.com>
2021-11-13 16:35:01 +0100

    Starting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { AuthComponent } from './components/auth/auth.component';
+import { ControlPanelComponent } from './components/control-panel/control-panel.component';
+import { RegisterComponent } from './components/auth/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavBarComponent,
+    FooterComponent,
+    AuthComponent,
+    ControlPanelComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
